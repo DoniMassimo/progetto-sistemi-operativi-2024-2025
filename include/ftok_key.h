6959d88167104ac5a65838c16053e643848e1fbf
DoniMassimo <donimassimo3@gmail.com>
2024-12-20 13:30:53 +0100

    feat: ftok key generation for seats mqueue

--- include/ftok_key.h
+++ include/ftok_key.h
@@ -9,6 +9,9 @@ extern key_t SEM_DAY_STARTED_KEY;
 extern key_t SHM_SEATS_INDEX_KEY;
 extern key_t SHM_SEATS_INFO_KEY;
 
+// each “seats” has a message queue that connects “user” with “worker”
+extern key_t* MSQ_SEATS_QUEUE_KEY;
+
 void ftok_key_init(void);
 
 #endif
--- src/ftok_key.c
+++ src/ftok_key.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include "ftok_key.h"
 #include "macros.h"
+#include "config.h"
 
 key_t key_count = 1;
 
@@ -12,6 +13,8 @@ key_t SEM_DAY_STARTED_KEY = -1;
 
 key_t SHM_SEATS_INDEX_KEY = -1;
 key_t SHM_SEATS_INFO_KEY = -1;
+
+key_t* MSQ_SEATS_QUEUE_KEY = NULL;
 
 void sem_key_init(void)
 {
@@ -33,8 +36,20 @@ void shm_key_init(void)
   if (-1 == SHM_SEATS_INFO_KEY) { FUNC_PERROR(); }
 }
 
+void msq_key_init(void)
+{
+  MSQ_SEATS_QUEUE_KEY = (key_t*)malloc(sizeof(key_t) * NOF_WORKER_SEATS);
+  if (NULL == MSQ_SEATS_QUEUE_KEY) { FUNC_PERROR(); }
+  for (int i = 0; i < NOF_WORKER_SEATS; i++)
+  {
+    MSQ_SEATS_QUEUE_KEY[i] = ftok(".", key_count++);
+    if (-1 == MSQ_SEATS_QUEUE_KEY[i]) { FUNC_PERROR(); }
+  }
+}
+
 void ftok_key_init(void)
 {
   sem_key_init();
   shm_key_init();
+  msq_key_init();
 }
